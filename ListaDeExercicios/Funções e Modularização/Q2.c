b7b68cf5355028c0427dba9e62c39c9755360fd9
livianlucena <livianmarialucena@gmail.com>
2025-05-07 23:28:13 -0300

    Update Q2.c
    Adicionando questão 2.

--- ListaDeExercicios/Funções e Modularização/Q2.c
+++ ListaDeExercicios/Funções e Modularização/Q2.c
@@ -1,31 +1,40 @@
-/*Potência (sem usar pow)
-  Implemente uma função int potencia(int base, int expoente) que calcule potências usando apenas multiplicações.*/
+/*
+Potência (sem usar pow)
+Implemente uma função int potencia(int base, int expoente) que calcule potências usando apenas multiplicações.
+*/
 
 #include <stdio.h>
 #include <locale.h>
 
-int potencia(int base, int expoente){
+// Função que calcula a potência de um número usando multiplicações
+int potencia(int base, int expoente) {
     int produto = 1;
-    for(int i = 0; i < expoente; i++){
+
+    // Multiplica a base por ela mesma "expoente" vezes
+    for (int i = 0; i < expoente; i++) {
         produto *= base;
     }
-    return produto;
+
+    return produto; // Retorna o resultado final
 }
 
-int main (){
-    setlocale(LC_ALL,"portuguese");
-
+int main() { 
     int base, expoente;
 
+    setlocale(LC_ALL, "portuguese"); 
+
+    // Entrada de dados
     printf("====== Calcular Potência ======\n\n");
     printf("Informe a base: ");
     scanf("%d", &base);
     printf("Informe o expoente: ");
     scanf("%d", &expoente);
 
+    // Chamada da função para calcular a potência
     int produto = potencia(base, expoente);
 
+    // Exibição do resultado
     printf("\nO valor de %d^%d = %d\n", base, expoente, produto);
 
-    return 0;
+    return 0; // Fim do programa
 }
