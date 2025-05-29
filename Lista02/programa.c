a99390e768ee2fb5d211cbb7fe78536f5a5dc0c5
livianlucena <livianmarialucena@gmail.com>
2025-05-29 08:38:37 -0300

    Update programa.c
    Adicionando comentários

--- Lista02/programa.c
+++ Lista02/programa.c
@@ -1,18 +1,23 @@
-#include <stdio.h>
-#include <locale.h>
+#include <stdio.h>   
 
+// Declaração da função x 
 int x(int);
 
-int main(){
-    int n = 10;
-    printf("%d\n", x(10));
-    return 0;
+int main() {
+    int n = 10;  // Define a posição da sequência de Fibonacci a ser calculada
+
+    // Chama a função x passando 10 e imprime o resultado
+    printf("%d\n", x(n));
+
+    return 0;  // Encerra o programa
 }
 
-int x (int n){
-    if(n <= 1){
-        return 1;
-    } else{
-        return x(n-1) + x(n-2);
+// Função recursiva que calcula o enésimo termo da sequência de Fibonacci
+int x(int n) {
+    if (n <= 1) {
+        return 1;  // Casos base: Fibonacci(0) = 1 e Fibonacci(1) = 1
+    } else {
+        // Chamada recursiva: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+        return x(n - 1) + x(n - 2);
     }
 }
